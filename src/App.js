e33d74689c13d1c937b274263900439984147892
Samuel Manoel <samuel@lojasantoantonio.com.br>
2021-07-18 21:05:07 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { useState } from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  const [value, setValue] = useState('')
+
+    const handleSubmit = (e)=>{
+    e.preventDefault(); 
+    if(value){
+      console.log(value)
+      localStorage.setItem('email',value)
+      setValue('')
+      toast.success(`${value}, cadastrado com sucesso!`)
+    }else{
+      toast.error(`Insira um e-mail`)
+      
+    }
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="main">
+            <div className="container">
+              <img src={logo} alt="" />
+              <p> A maior Black Friday vem ai! </p>
+              <p>Deixa seu melhor e-mail a baixo e fique por dentro das promoções.</p>
+              <div>
+                <form onSubmit={handleSubmit}>
+                <input type="email" placeholder="Digite seu melhor e-mail" value={value} onChange={e => setValue(e.target.value)} autoFocus={true} className="input"/>
+                <button type="submit" className="button">Cadastrar</button>
+                </form>
+                <ToastContainer />
+              </div>
+              </div>
+      </main>
+    </>
   );
 }
 
